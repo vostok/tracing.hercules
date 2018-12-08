5e6655c5cb2026f151a79f0fc2e0b9e5c137bea9
iloktionov <iloktionov@skbkontur.ru>
2018-12-08 17:00:49 +0500

    Implemented HerculesSpanSender.

--- Vostok.Tracing.Hercules/HerculesSpanSender.cs
+++ Vostok.Tracing.Hercules/HerculesSpanSender.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using JetBrains.Annotations;
+using Vostok.Commons.Formatting;
+using Vostok.Commons.Time;
+using Vostok.Hercules.Client.Abstractions;
+using Vostok.Hercules.Client.Abstractions.Events;
 using Vostok.Tracing.Abstractions;
 
 namespace Vostok.Tracing.Hercules
 {
+    /// <summary>
+    /// An implementation of <see cref="ISpanSender"/> based on <see cref="IHerculesSink"/> from Hercules client library.
+    /// </summary>
     [PublicAPI]
     public class HerculesSpanSender : ISpanSender
     {
@@ -16,7 +23,42 @@ namespace Vostok.Tracing.Hercules
 
         public void Send(ISpan span)
         {
-            throw new NotImplementedException();
+            config.Sink.Put(config.Stream, builder => BuildHerculesEvent(builder, span));
+        }
+
+        private void BuildHerculesEvent(IHerculesEventBuilder builder, ISpan span)
+        {
+            builder.SetTimestamp(span.EndTimestamp ?? span.BeginTimestamp);
+
+            builder
+                .AddValue(TagNames.TraceId, span.TraceId)
+                .AddValue(TagNames.SpanId, span.SpanId)
+                .AddValue(TagNames.BeginTimestampUtc, EpochHelper.ToGregorianUtcTicks(span.BeginTimestamp.UtcDateTime))
+                .AddValue(TagNames.BeginTimestampUtcOffset, span.BeginTimestamp.Offset.Ticks);
+
+            if (span.ParentSpanId.HasValue)
+            {
+                builder.AddValue(TagNames.ParentSpanId, span.ParentSpanId.Value);
+            }
+
+            if (span.EndTimestamp.HasValue)
+            {
+                builder.AddValue(TagNames.EndTimestampUtc, EpochHelper.ToGregorianUtcTicks(span.EndTimestamp.Value.UtcDateTime));
+                builder.AddValue(TagNames.EndTimestampUtcOffset, span.EndTimestamp.Value.Offset.Ticks);
+            }
+
+            builder.AddContainer(TagNames.Annotations, tagBuilder => BuildAnnotationsContainer(tagBuilder, span));
+        }
+
+        private void BuildAnnotationsContainer(IHerculesTagsBuilder builder, ISpan span)
+        {
+            foreach (var pair in span.Annotations)
+            {
+                if (builder.TryAddObject(pair.Key, pair.Value))
+                    continue;
+
+                builder.AddValue(pair.Key, ObjectValueFormatter.Format(pair.Value, formatProvider: config.FormatProvider));
+            }
         }
     }
 }
---
+++ Vostok.Tracing.Hercules/TagNames.cs
@@ -0,0 +1,21 @@
+﻿namespace Vostok.Tracing.Hercules
+{
+    internal static class TagNames
+    {
+        public const string TraceId = nameof(TraceId);
+
+        public const string SpanId = nameof(SpanId);
+
+        public const string ParentSpanId = nameof(ParentSpanId);
+
+        public const string BeginTimestampUtc = nameof(BeginTimestampUtc);
+
+        public const string BeginTimestampUtcOffset = nameof(BeginTimestampUtcOffset);
+
+        public const string EndTimestampUtc = nameof(EndTimestampUtc);
+
+        public const string EndTimestampUtcOffset = nameof(EndTimestampUtcOffset);
+
+        public const string Annotations = nameof(Annotations);
+    }
+}
