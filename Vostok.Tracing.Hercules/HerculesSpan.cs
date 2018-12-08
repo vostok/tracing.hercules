6b63870cb5b0fe139174fce0c77eb0c252bd1a50
iloktionov <iloktionov@skbkontur.ru>
2018-12-08 18:16:56 +0500

    Implemented HerculesSpan.

--- Vostok.Tracing.Hercules/HerculesSpan.cs
+++ Vostok.Tracing.Hercules/HerculesSpan.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using Vostok.Commons.Time;
 using Vostok.Hercules.Client.Abstractions.Events;
 using Vostok.Tracing.Abstractions;
+
+// ReSharper disable PossibleInvalidOperationException
 
 namespace Vostok.Tracing.Hercules
 {
@@ -10,22 +13,44 @@ namespace Vostok.Tracing.Hercules
     public class HerculesSpan : ISpan
     {
         private readonly HerculesEvent @event;
+        private volatile HerculesSpanAnnotations annotations;
 
         public HerculesSpan([NotNull] HerculesEvent @event)
         {
             this.@event = @event ?? throw new ArgumentNullException(nameof(@event));
         }
 
-        public Guid TraceId => throw new NotImplementedException();
+        public Guid TraceId
+            => @event.Tags.GetValue(TagNames.TraceId).AsGuid;
 
-        public Guid SpanId => throw new NotImplementedException();
+        public Guid SpanId
+            => @event.Tags.GetValue(TagNames.SpanId).AsGuid;
 
-        public Guid? ParentSpanId => throw new NotImplementedException();
+        public Guid? ParentSpanId
+            => @event.Tags[TagNames.ParentSpanId]?.AsGuid;
 
-        public DateTimeOffset BeginTimestamp => throw new NotImplementedException();
+        public DateTimeOffset BeginTimestamp
+            => ExtractTimestamp(@event, TagNames.BeginTimestampUtc, TagNames.BeginTimestampUtcOffset).Value;
 
-        public DateTimeOffset? EndTimestamp => throw new NotImplementedException();
+        public DateTimeOffset? EndTimestamp
+            => ExtractTimestamp(@event, TagNames.EndTimestampUtc, TagNames.EndTimestampUtcOffset);
 
-        public IReadOnlyDictionary<string, object> Annotations => throw new NotImplementedException();
+        public IReadOnlyDictionary<string, object> Annotations
+            => annotations ?? (annotations = new HerculesSpanAnnotations(@event.Tags.GetValue(TagNames.Annotations).AsContainer));
+
+        [CanBeNull]
+        private static DateTimeOffset? ExtractTimestamp(HerculesEvent @event, string timestampTag, string offsetTag)
+        {
+            if (!@event.Tags.TryGetValue(timestampTag, out var timestampValue) ||
+                !@event.Tags.TryGetValue(offsetTag, out var offsetValue))
+            {
+                return null;
+            }
+
+            var utcTimestamp = EpochHelper.FromGregorianUtcTicks(timestampValue.AsLong);
+            var utcOffset = TimeSpan.FromTicks(offsetValue.AsLong);
+
+            return new DateTimeOffset(DateTime.SpecifyKind(utcTimestamp + utcOffset, DateTimeKind.Unspecified), utcOffset);
+        }
     }
 }
