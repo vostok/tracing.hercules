ef171a864b0d461eff8fd9f89b671a834cc3132f
iloktionov <iloktionov@skbkontur.ru>
2018-12-08 16:42:55 +0500

    Stubs for public APIs.

---
+++ Vostok.Tracing.Hercules/HerculesSpan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Tracing.Abstractions;
+
+namespace Vostok.Tracing.Hercules
+{
+    [PublicAPI]
+    public class HerculesSpan : ISpan
+    {
+        private readonly HerculesEvent @event;
+
+        public HerculesSpan([NotNull] HerculesEvent @event)
+        {
+            this.@event = @event ?? throw new ArgumentNullException(nameof(@event));
+        }
+
+        public Guid TraceId => throw new NotImplementedException();
+
+        public Guid SpanId => throw new NotImplementedException();
+
+        public Guid? ParentSpanId => throw new NotImplementedException();
+
+        public DateTimeOffset BeginTimestamp => throw new NotImplementedException();
+
+        public DateTimeOffset? EndTimestamp => throw new NotImplementedException();
+
+        public IReadOnlyDictionary<string, object> Annotations => throw new NotImplementedException();
+    }
+}
---
+++ Vostok.Tracing.Hercules/HerculesSpanSender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Hercules.Client.Abstractions;
+using Vostok.Tracing.Abstractions;
+
+namespace Vostok.Tracing.Hercules
+{
+    [PublicAPI]
+    public class HerculesSpanSender : ISpanSender
+    {
+        private readonly IHerculesSink sink;
+        private readonly string stream;
+
+        public HerculesSpanSender([NotNull] IHerculesSink sink, [NotNull] string stream)
+        {
+            this.sink = sink ?? throw new ArgumentNullException(nameof(sink));
+            this.stream = stream ?? throw new ArgumentNullException(nameof(stream));
+        }
+
+        public void Send(ISpan span)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
