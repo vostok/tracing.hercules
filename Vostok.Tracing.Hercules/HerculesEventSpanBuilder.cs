1dd2882ecbf787fa90aa7d0d2838e1b7e0651e8a
kungurtsev <kungurtsev@skbkontur.ru>
2019-08-01 13:41:33 +0500

    extract builder

---
+++ Vostok.Tracing.Hercules/HerculesEventSpanBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Commons.Formatting;
+using Vostok.Commons.Time;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Tracing.Abstractions;
+
+namespace Vostok.Tracing.Hercules
+{
+    /// <summary>
+    /// Converts <see cref="ISpan"/>s to <see cref="HerculesEvent"/>.
+    /// </summary>
+    [PublicAPI]
+    public class HerculesEventSpanBuilder
+    {
+        public static HerculesEvent Build([NotNull] ISpan span, IFormatProvider formatProvider)
+        {
+            var builder = new HerculesEventBuilder();
+            Build(span, builder, formatProvider);
+            return builder.BuildEvent();
+        }
+
+        public static void Build([NotNull] ISpan span, [NotNull] IHerculesEventBuilder builder, IFormatProvider formatProvider)
+        {
+            builder.SetTimestamp(span.EndTimestamp ?? span.BeginTimestamp);
+
+            builder
+                .AddValue(TagNames.TraceId, span.TraceId)
+                .AddValue(TagNames.SpanId, span.SpanId)
+                .AddValue(TagNames.BeginTimestampUtc, EpochHelper.ToUnixTimeUtcTicks(span.BeginTimestamp.UtcDateTime))
+                .AddValue(TagNames.BeginTimestampUtcOffset, span.BeginTimestamp.Offset.Ticks);
+
+            if (span.ParentSpanId.HasValue)
+            {
+                builder.AddValue(TagNames.ParentSpanId, span.ParentSpanId.Value);
+            }
+
+            if (span.EndTimestamp.HasValue)
+            {
+                builder.AddValue(TagNames.EndTimestampUtc, EpochHelper.ToUnixTimeUtcTicks(span.EndTimestamp.Value.UtcDateTime));
+                builder.AddValue(TagNames.EndTimestampUtcOffset, span.EndTimestamp.Value.Offset.Ticks);
+            }
+
+            builder.AddContainer(TagNames.Annotations, tagBuilder => BuildAnnotationsContainer(span, tagBuilder, formatProvider));
+        }
+
+        private static void BuildAnnotationsContainer(ISpan span, IHerculesTagsBuilder builder, IFormatProvider formatProvider)
+        {
+            foreach (var pair in span.Annotations)
+            {
+                if (builder.TryAddObject(pair.Key, pair.Value))
+                    continue;
+
+                builder.AddValue(pair.Key, ObjectValueFormatter.Format(pair.Value, formatProvider: formatProvider));
+            }
+        }
+    }
+}
--- Vostok.Tracing.Hercules/HerculesSpanSender.cs
+++ Vostok.Tracing.Hercules/HerculesSpanSender.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using JetBrains.Annotations;
-using Vostok.Commons.Formatting;
-using Vostok.Commons.Time;
 using Vostok.Hercules.Client.Abstractions;
-using Vostok.Hercules.Client.Abstractions.Events;
 using Vostok.Tracing.Abstractions;
 
 namespace Vostok.Tracing.Hercules
@@ -30,42 +27,7 @@ namespace Vostok.Tracing.Hercules
         {
             var settings = settingsProvider();
 
-            settings.Sink.Put(settings.Stream, builder => BuildHerculesEvent(builder, span, settings.FormatProvider));
-        }
-
-        private void BuildHerculesEvent(IHerculesEventBuilder builder, ISpan span, IFormatProvider formatProvider)
-        {
-            builder.SetTimestamp(span.EndTimestamp ?? span.BeginTimestamp);
-
-            builder
-                .AddValue(TagNames.TraceId, span.TraceId)
-                .AddValue(TagNames.SpanId, span.SpanId)
-                .AddValue(TagNames.BeginTimestampUtc, EpochHelper.ToUnixTimeUtcTicks(span.BeginTimestamp.UtcDateTime))
-                .AddValue(TagNames.BeginTimestampUtcOffset, span.BeginTimestamp.Offset.Ticks);
-
-            if (span.ParentSpanId.HasValue)
-            {
-                builder.AddValue(TagNames.ParentSpanId, span.ParentSpanId.Value);
-            }
-
-            if (span.EndTimestamp.HasValue)
-            {
-                builder.AddValue(TagNames.EndTimestampUtc, EpochHelper.ToUnixTimeUtcTicks(span.EndTimestamp.Value.UtcDateTime));
-                builder.AddValue(TagNames.EndTimestampUtcOffset, span.EndTimestamp.Value.Offset.Ticks);
-            }
-
-            builder.AddContainer(TagNames.Annotations, tagBuilder => BuildAnnotationsContainer(tagBuilder, span, formatProvider));
-        }
-
-        private void BuildAnnotationsContainer(IHerculesTagsBuilder builder, ISpan span, IFormatProvider formatProvider)
-        {
-            foreach (var pair in span.Annotations)
-            {
-                if (builder.TryAddObject(pair.Key, pair.Value))
-                    continue;
-
-                builder.AddValue(pair.Key, ObjectValueFormatter.Format(pair.Value, formatProvider: formatProvider));
-            }
+            settings.Sink.Put(settings.Stream, builder => HerculesEventSpanBuilder.Build(span, builder, settings.FormatProvider));
         }
     }
 }
