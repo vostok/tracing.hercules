faa3b49dcbb1c1dec7e1ad5ec183c978f1fec13b
kungurtsev <kungurtsev@skbkontur.ru>
2020-03-20 12:41:41 +0500

    HttpClientSpan

---
+++ Vostok.Tracing.Hercules/HerculesHttpClientSpan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Tracing.Hercules
+{
+    [PublicAPI]
+    public class HerculesHttpClientSpan
+    {
+        public Guid TraceId { get; set; }
+
+        public DateTimeOffset BeginTimestamp { get; set; }
+
+        public DateTimeOffset EndTimestamp { get; set; }
+
+        public string Application { get; set; }
+
+        public string Host { get; set; }
+
+        public string TargetEnvironment { get; set; }
+
+        public string TargetService { get; set; }
+
+        public string RequestMethod { get; set; }
+
+        public int? RequestSize { get; set; }
+
+        public int? ResponseCode { get; set; }
+
+        public int? ResponseSize { get; set; }
+
+        public Uri Url { get; set; }
+    }
+}
