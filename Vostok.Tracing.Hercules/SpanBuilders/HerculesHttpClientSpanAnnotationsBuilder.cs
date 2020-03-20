92cc2c02169b6605aaa8a2dfd0793abe247aa23d
kungurtsev <kungurtsev@skbkontur.ru>
2020-03-20 12:41:41 +0500

    HttpClientSpan

---
+++ Vostok.Tracing.Hercules/SpanBuilders/HerculesHttpClientSpanAnnotationsBuilder.cs
@@ -0,0 +1,61 @@
+﻿using Vostok.Commons.Helpers.Url;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Tracing.Abstractions;
+
+namespace Vostok.Tracing.Hercules.SpanBuilders
+{
+    internal class HerculesHttpClientSpanAnnotationsBuilder : DummyHerculesTagsBuilder, IHerculesTagsBuilder
+    {
+        private readonly HerculesHttpClientSpan span;
+
+        public HerculesHttpClientSpanAnnotationsBuilder(HerculesHttpClientSpan span)
+        {
+            this.span = span;
+        }
+
+        public new IHerculesTagsBuilder AddValue(string key, string value)
+        {
+            switch (key)
+            {
+                case WellKnownAnnotations.Common.Application:
+                    span.Application = value;
+                    break;
+                case WellKnownAnnotations.Common.Host:
+                    span.Host = value;
+                    break;
+                case WellKnownAnnotations.Http.Request.Url:
+                    span.Url = UrlParser.Parse(value);
+                    break;
+                case WellKnownAnnotations.Http.Request.Method:
+                    span.RequestMethod = value;
+                    break;
+                case WellKnownAnnotations.Http.Request.TargetEnvironment:
+                    span.TargetEnvironment = value;
+                    break;
+                case WellKnownAnnotations.Http.Request.TargetService:
+                    span.TargetService = value;
+                    break;
+            }
+
+            return this;
+        }
+
+        public new IHerculesTagsBuilder AddValue(string key, int value)
+        {
+            switch (key)
+            {
+                case WellKnownAnnotations.Http.Request.Size:
+                    span.RequestSize = value;
+                    break;
+                case WellKnownAnnotations.Http.Response.Code:
+                    span.ResponseCode = value;
+                    break;
+                case WellKnownAnnotations.Http.Response.Size:
+                    span.ResponseSize = value;
+                    break;
+            }
+
+            return this;
+        }
+    }
+}
---
+++ Vostok.Tracing.Hercules/SpanBuilders/HerculesHttpClientSpanBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Commons.Time;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Tracing.Hercules.Helpers;
+
+namespace Vostok.Tracing.Hercules.SpanBuilders
+{
+    [PublicAPI]
+    public class HerculesHttpClientSpanBuilder : DummyHerculesTagsBuilder, IHerculesEventBuilder<HerculesHttpClientSpan>
+    {
+        private static readonly DummyHerculesTagsBuilder DummyBuilder = new DummyHerculesTagsBuilder();
+        private readonly HerculesHttpClientSpan span = new HerculesHttpClientSpan();
+
+        public IHerculesEventBuilder<HerculesHttpClientSpan> SetTimestamp(DateTimeOffset timestamp) => this;
+
+        public HerculesHttpClientSpan BuildEvent() => span;
+
+        public new IHerculesTagsBuilder AddValue(string key, Guid value)
+        {
+            switch (key)
+            {
+                case TagNames.TraceId:
+                    span.TraceId = value;
+                    break;
+            }
+
+            return this;
+        }
+
+        public new IHerculesTagsBuilder AddValue(string key, long value)
+        {
+            switch (key)
+            {
+                case TagNames.BeginTimestampUtc:
+                    span.BeginTimestamp = EpochHelper.FromUnixTimeUtcTicks(value);
+                    break;
+                case TagNames.EndTimestampUtc:
+                    span.EndTimestamp = EpochHelper.FromUnixTimeUtcTicks(value);
+                    break;
+                case TagNames.BeginTimestampUtcOffset:
+                    span.BeginTimestamp = new DateTimeOffset(span.BeginTimestamp.DateTime, TimeSpan.FromTicks(value));
+                    break;
+                case TagNames.EndTimestampUtcOffset:
+                    span.EndTimestamp = new DateTimeOffset(span.EndTimestamp.DateTime, TimeSpan.FromTicks(value));
+                    break;
+            }
+
+            return this;
+        }
+
+        public new IHerculesTagsBuilder AddContainer(string key, Action<IHerculesTagsBuilder> valueBuilder)
+        {
+            valueBuilder(
+                key == TagNames.Annotations
+                    ? new HerculesHttpClientSpanAnnotationsBuilder(span)
+                    : DummyBuilder);
+
+            return this;
+        }
+    }
+}
