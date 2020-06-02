e5a6e492bd722d1991336e0775f5e9541b7666a3
kungurtsev <kungurtsev@skbkontur.ru>
2020-06-02 12:46:07 +0500

    move ResponseCode to HttpSpan

--- Vostok.Tracing.Hercules/Models/HerculesHttpClientSpan.cs
+++ Vostok.Tracing.Hercules/Models/HerculesHttpClientSpan.cs
@@ -1,14 +1,11 @@
 ﻿using JetBrains.Annotations;
-using Vostok.Clusterclient.Core.Model;
 
 namespace Vostok.Tracing.Hercules.Models
 {
     [PublicAPI]
     public class HerculesHttpClientSpan : HerculesHttpSpan
     {
-        public ResponseCode ResponseCode { get; set; }
-
         public override string ToString() =>
-            $"{base.ToString()}, {nameof(ResponseCode)}: {ResponseCode}";
+            $"{base.ToString()}";
     }
 }
--- Vostok.Tracing.Hercules/Models/HerculesHttpSpan.cs
+++ Vostok.Tracing.Hercules/Models/HerculesHttpSpan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using JetBrains.Annotations;
+using Vostok.Clusterclient.Core.Model;
 
 namespace Vostok.Tracing.Hercules.Models
 {
@@ -28,9 +29,11 @@ namespace Vostok.Tracing.Hercules.Models
 
         public long? ResponseSize { get; set; }
 
+        public ResponseCode ResponseCode { get; set; }
+        
         public Uri RequestUrl { get; set; }
 
         public override string ToString() =>
-            $"{nameof(TraceId)}: {TraceId}, {nameof(BeginTimestamp)}: {BeginTimestamp}, {nameof(EndTimestamp)}: {EndTimestamp}, {nameof(Latency)}: {Latency}, {nameof(Application)}: {Application}, {nameof(Host)}: {Host}, {nameof(TargetEnvironment)}: {TargetEnvironment}, {nameof(TargetService)}: {TargetService}, {nameof(RequestMethod)}: {RequestMethod}, {nameof(RequestSize)}: {RequestSize}, {nameof(ResponseSize)}: {ResponseSize}, {nameof(RequestUrl)}: {RequestUrl}";
+            $"{nameof(TraceId)}: {TraceId}, {nameof(BeginTimestamp)}: {BeginTimestamp}, {nameof(EndTimestamp)}: {EndTimestamp}, {nameof(Latency)}: {Latency}, {nameof(Application)}: {Application}, {nameof(Host)}: {Host}, {nameof(TargetEnvironment)}: {TargetEnvironment}, {nameof(TargetService)}: {TargetService}, {nameof(RequestMethod)}: {RequestMethod}, {nameof(RequestSize)}: {RequestSize}, {nameof(ResponseSize)}: {ResponseSize}, {nameof(ResponseCode)}: {ResponseCode}, {nameof(RequestUrl)}: {RequestUrl}";
     }
 }
