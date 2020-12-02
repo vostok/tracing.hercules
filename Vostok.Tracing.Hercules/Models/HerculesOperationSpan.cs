9096b2c67ff0f903966f4279b15b59ebccfc0392
romanbykov <romanbykov@skbkontur.ru>
2020-12-02 18:36:47 +0500

    add operation model

---
+++ Vostok.Tracing.Hercules/Models/HerculesOperationSpan.cs
@@ -0,0 +1,19 @@
+using JetBrains.Annotations;
+
+namespace Vostok.Tracing.Hercules.Models
+{
+    [PublicAPI]
+    public class HerculesOperationSpan: HerculesCommonSpan
+    {
+        public string TargetEnvironment { get; set; }
+        
+        public string TargetService { get; set; }
+        
+        public string CustomStatus { get; set; }
+        
+        public long? Size { get; set; }
+
+        public override string ToString() =>
+            $"{base.ToString()}, {nameof(TargetEnvironment)}: {TargetEnvironment}, {nameof(TargetService)}: {TargetService}, {nameof(CustomStatus)}: {CustomStatus}, {nameof(Size)}: {Size}";
+    }
+}
