a69b6341d7592cea02a9b58ec2810c3b1d28251d
kungurtsev <kungurtsev@skbkontur.ru>
2020-12-03 13:07:44 +0500

    + test

---
+++ Vostok.Tracing.Hercules.Tests/SpanReadersTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Tracing.Abstractions;
+using Vostok.Tracing.Hercules.Readers;
+
+namespace Vostok.Tracing.Hercules.Tests
+{
+    [TestFixture]
+    internal class SpanReadersTests
+    {
+        [Test]
+        public void Should_fill_annotations()
+        {
+            var reader = new HerculesHttpClusterSpanReader();
+            reader.AddValue(WellKnownAnnotations.Http.Request.TargetEnvironment, "a");
+            
+            var span = reader.BuildEvent();
+            span.TargetEnvironment.Should().Be("a");
+        }
+    }
+}
