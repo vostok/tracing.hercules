b048df2fc3b10e19513f56d1297edcc7c4dd6feb
kungurtsev <kungurtsev@skbkontur.ru>
2021-02-28 13:26:21 +0500

    fix HerculesCustomClusterSpan sizes

--- Vostok.Tracing.Hercules/Readers/AnnotationReaders/HerculesCustomClusterSpanAnnotationsReader.cs
+++ Vostok.Tracing.Hercules/Readers/AnnotationReaders/HerculesCustomClusterSpanAnnotationsReader.cs
@@ -1,12 +1,35 @@
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Tracing.Abstractions;
 using Vostok.Tracing.Hercules.Models;
 
 namespace Vostok.Tracing.Hercules.Readers.AnnotationReaders
 {
     internal class HerculesCustomClusterSpanAnnotationsReader : HerculesCustomSpanAnnotationsReader
     {
+        private readonly HerculesCustomClusterSpan span;
+
         public HerculesCustomClusterSpanAnnotationsReader(HerculesCustomClusterSpan span)
             : base(span)
         {
+            this.span = span;
+        }
+        
+        public new IHerculesTagsBuilder AddValue(string key, long value)
+        {
+            switch (key)
+            {
+                case WellKnownAnnotations.Custom.Request.Size:
+                    span.RequestSize = value;
+                    break;
+                case WellKnownAnnotations.Custom.Response.Size:
+                    span.ResponseSize = value;
+                    break;
+                default:
+                    base.AddValue(key, value);
+                    break;
+            }
+
+            return this;
         }
     }
 }
