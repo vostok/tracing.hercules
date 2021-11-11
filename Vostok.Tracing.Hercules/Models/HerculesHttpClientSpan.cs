9653ea57cc9d4a1c7ce8b05edc6bcd9fc1037aa2
Ilya Tzoop <ilyatzoop@gmail.com>
2021-11-11 14:40:02 +0500

    Return overriden ToString

--- Vostok.Tracing.Hercules/Models/HerculesHttpClientSpan.cs
+++ Vostok.Tracing.Hercules/Models/HerculesHttpClientSpan.cs
@@ -5,5 +5,7 @@ namespace Vostok.Tracing.Hercules.Models
     [PublicAPI]
     public class HerculesHttpClientSpan : HerculesHttpSpan
     {
+        public override string ToString() =>
+            $"{base.ToString()}";
     }
 }
