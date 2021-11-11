642c0f2478dad754d805c5f4d7223bf0a1c03b5f
Ilya Tzoop <ilyatzoop@gmail.com>
2021-11-11 11:01:47 +0500

    Add HerculesHttpServerSpan and readers for it

--- Vostok.Tracing.Hercules/Models/HerculesHttpClientSpan.cs
+++ Vostok.Tracing.Hercules/Models/HerculesHttpClientSpan.cs
@@ -5,7 +5,5 @@ namespace Vostok.Tracing.Hercules.Models
     [PublicAPI]
     public class HerculesHttpClientSpan : HerculesHttpSpan
     {
-        public override string ToString() =>
-            $"{base.ToString()}";
     }
 }
---
+++ Vostok.Tracing.Hercules/Readers/HerculesHttpServerSpanReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Tracing.Hercules.Helpers;
+using Vostok.Tracing.Hercules.Models;
+using Vostok.Tracing.Hercules.Readers.AnnotationReaders;
+
+namespace Vostok.Tracing.Hercules.Readers
+{
+    public class HerculesHttpServerSpanReader : HerculesCommonSpanReader, IHerculesEventBuilder<HerculesHttpServerSpan>
+    {
+        private static readonly DummyHerculesTagsBuilder DummyBuilder = new DummyHerculesTagsBuilder();
+        private readonly HerculesHttpServerSpan span;
+        
+        public HerculesHttpServerSpanReader()
+            : this(new HerculesHttpServerSpan())
+        {
+        }
+
+        public HerculesHttpServerSpanReader(HerculesHttpServerSpan span)
+            : base(span)
+        {
+            this.span = span;
+        }
+
+        public new IHerculesEventBuilder<HerculesHttpServerSpan> SetTimestamp(DateTimeOffset timestamp) => this;
+
+        public new HerculesHttpServerSpan BuildEvent() => span;
+        
+        public new IHerculesTagsBuilder AddContainer(string key, Action<IHerculesTagsBuilder> valueBuilder)
+        {
+            valueBuilder(
+                key == TagNames.Annotations
+                    ? new HerculesHttpServerSpanAnnotationsReader(span)
+                    : DummyBuilder);
+
+            return this;
+        }
+    }
+}
