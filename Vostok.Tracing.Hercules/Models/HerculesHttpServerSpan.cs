029fd26dfcf4666f601859ef174e12bded33b4d8
Ilya Tzoop <ilyatzoop@gmail.com>
2021-11-11 11:01:47 +0500

    Add HerculesHttpServerSpan and readers for it

---
+++ Vostok.Tracing.Hercules/Models/HerculesHttpServerSpan.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using JetBrains.Annotations;
+
+namespace Vostok.Tracing.Hercules.Models
+{
+    [PublicAPI]
+    public class HerculesHttpServerSpan : HerculesHttpSpan
+    {
+        public string ClientName { get; set; }
+
+        public IPAddress ClientAddress { get; set; }
+
+        public override string ToString() =>
+            $"{base.ToString()}, {nameof(ClientName)}: {ClientName}, {nameof(ClientAddress)}: {ClientAddress}";
+    }
+}
