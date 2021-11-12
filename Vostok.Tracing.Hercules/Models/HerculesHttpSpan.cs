ee717122411c2f49ee6df32ae6ffaa40b36ead42
Ilya Tzoop <ilyatzoop@gmail.com>
2021-11-12 15:04:54 +0500

    Added HttpBaseSpan. Inherited HttpSpan from it. Added readers to it.

--- Vostok.Tracing.Hercules/Models/HerculesHttpSpan.cs
+++ Vostok.Tracing.Hercules/Models/HerculesHttpSpan.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using JetBrains.Annotations;
-using Vostok.Clusterclient.Core.Model;
+﻿using JetBrains.Annotations;
 
 namespace Vostok.Tracing.Hercules.Models
 {
     [PublicAPI]
-    public abstract class HerculesHttpSpan : HerculesCommonSpan
+    public abstract class HerculesHttpSpan : HerculesHttpBaseSpan
     {
         public string TargetEnvironment { get; set; }
 
         public string TargetService { get; set; }
 
-        public string RequestMethod { get; set; }
-
-        public long? RequestSize { get; set; }
-
-        public long? ResponseSize { get; set; }
-
-        public ResponseCode ResponseCode { get; set; }
-
-        public Uri RequestUrl { get; set; }
-
         public override string ToString() =>
-            $"{nameof(TraceId)}: {TraceId}, {nameof(BeginTimestamp)}: {BeginTimestamp}, {nameof(EndTimestamp)}: {EndTimestamp}, {nameof(Latency)}: {Latency}, {nameof(Application)}: {Application}, {nameof(Environment)}: {Environment}, {nameof(Host)}: {Host}, {nameof(TargetEnvironment)}: {TargetEnvironment}, {nameof(TargetService)}: {TargetService}, {nameof(RequestMethod)}: {RequestMethod}, {nameof(RequestSize)}: {RequestSize}, {nameof(ResponseSize)}: {ResponseSize}, {nameof(ResponseCode)}: {ResponseCode}, {nameof(RequestUrl)}: {RequestUrl}";
+            $"{base.ToString()}, {nameof(TargetEnvironment)}: {TargetEnvironment}, {nameof(TargetService)}: {TargetService}";
     }
 }
---
+++ Vostok.Tracing.Hercules/Readers/AnnotationReaders/HerculesHttpBaseSpanAnnotationsReader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Vostok.Clusterclient.Core.Model;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Tracing.Abstractions;
+using Vostok.Tracing.Hercules.Models;
+
+namespace Vostok.Tracing.Hercules.Readers.AnnotationReaders
+{
+    internal abstract class HerculesHttpBaseSpanAnnotationsReader : HerculesCommonSpanAnnotationsReader, IHerculesTagsBuilder
+    {
+        private readonly HerculesHttpBaseSpan span;
+
+        protected HerculesHttpBaseSpanAnnotationsReader(HerculesHttpBaseSpan span)
+            : base(span) =>
+            this.span = span;
+
+        public new IHerculesTagsBuilder AddValue(string key, string value)
+        {
+            switch (key)
+            {
+                case WellKnownAnnotations.Http.Request.Url:
+                    span.RequestUrl = !Uri.TryCreate(value, UriKind.RelativeOrAbsolute, out var parsed) ? null : parsed;
+                    break;
+                case WellKnownAnnotations.Http.Request.Method:
+                    span.RequestMethod = value;
+                    break;
+                default:
+                    base.AddValue(key, value);
+                    break;
+            }
+
+            return this;
+        }
+
+        public new IHerculesTagsBuilder AddValue(string key, long value)
+        {
+            switch (key)
+            {
+                case WellKnownAnnotations.Http.Request.Size:
+                    span.RequestSize = value;
+                    break;
+                case WellKnownAnnotations.Http.Response.Size:
+                    span.ResponseSize = value;
+                    break;
+                default:
+                    base.AddValue(key, value);
+                    break;
+            }
+
+            return this;
+        }
+
+        public new IHerculesTagsBuilder AddValue(string key, int value)
+        {
+            switch (key)
+            {
+                case WellKnownAnnotations.Http.Response.Code:
+                    span.ResponseCode = (ResponseCode)value;
+                    break;
+                default:
+                    base.AddValue(key, value);
+                    break;
+            }
+
+            return this;
+        }
+    }
+}
