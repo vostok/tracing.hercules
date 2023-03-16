5f711d07d3764b9e4bc3b3348e9b2ca7f39a81b1
kungurtsev <kungurtsev@skbkontur.ru>
2023-03-16 12:47:11 +0400

    Revert "add activity kinds constants"
    This reverts commit a4d699c1d8492172316d85e358b9ffc59578b577.

--- Vostok.Tracing.Hercules/Readers/OpenTelemetrySemanticConventions.cs
+++ Vostok.Tracing.Hercules/Readers/OpenTelemetrySemanticConventions.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-namespace Vostok.Tracing.Hercules.Readers;
+﻿namespace Vostok.Tracing.Hercules.Readers;
 
 internal static class OpenTelemetrySemanticConventions
 {
@@ -13,9 +11,4 @@ internal static class OpenTelemetrySemanticConventions
     public const string HttpClientIp = "http.client_ip";
     public const string HttpRequestContentLength = "http.request_content_length";
     public const string HttpResponseContentLength = "http.response_content_length";
-
-    public const string ActivityKindServer = nameof(ActivityKind.Server);
-    public const string ActivityKindClient = nameof(ActivityKind.Client);
-    public const string ActivityStatusCodeOk = nameof(ActivityStatusCode.Ok);
-    public const string ActivityStatusCodeError = nameof(ActivityStatusCode.Error);
 }
